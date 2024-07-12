f6b2b90780a9e983023a30c2157645b8efbde7d5
SarveshP12 <sarvesh.2005.1201@gmail.com>
2024-07-12 12:31:03 +0530

    adding components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router, Routes, Route, Link
+// } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState();
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      document.body.style.backgroundColor = '#042743';
+      setMode('dark');
+      showAlert("Dark mode Enabled", "success");
+      document.title = "TextUtils - DarkMode";
+      setInterval(() => {
+        document.title = 'TextUtils is amazing!!!';
+      }, 2000);
+      setInterval(() => {
+        document.title = 'Install TextUtils!!';
+      }, 1500);
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode Enabled", "danger");
+      document.title = "TextUtils - LightMode";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar mode={mode} title="Sarvesh" aboutText="About Us" toggleMode={toggleMode} />
+    <Alert alert={alert} />
+    <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />
+      {/* <Router>
+        <Navbar mode={mode} title="Sarvesh" aboutText="About Us" toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} toggleMode={toggleMode} />} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router> */}
+    </>
   );
 }
 
